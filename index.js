2eb236882b149604e86344932543e31c5f152d2a
Greg Ejimofor <grgonyx@gmail.com>
2023-04-15 14:32:06 -0700

    commit tree 76184943408ebc1bc425dbaf41464cfb8e6501af

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name: "Greg Ejimofor",
+    streetAddress: "14 Nguru Rd",
+};
+function updateEmployeeWithKeyAndValue(obj,key,value ){
+    const newObj = {...obj};
+    newObj[key] = value;
+    return newObj;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]  = value;
+    return obj;
+}
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = {...obj};
+    delete newObj.name;
+    return newObj;
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj.name;
+    return obj;
+}
